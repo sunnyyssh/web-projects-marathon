fd66be05aa4d3b732811c778dcd7a7743736a405
sunnyyssh <blit228@mail.ru>
2024-08-03 17:23:22 +0300

    Added DbContext to Host with Quiz model.

---
+++ 4-quiz/Quiz.Host/Migrations/20240803125500_Initial.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quiz.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quizzes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quizzes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Question = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Options = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QuizInfoId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Questions_Quizzes_QuizInfoId",
+                        column: x => x.QuizInfoId,
+                        principalTable: "Quizzes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuizCorrectAnswers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuizId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuizCorrectAnswers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuizCorrectAnswers_Quizzes_QuizId",
+                        column: x => x.QuizId,
+                        principalTable: "Quizzes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuestionCorrectAnswers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuestionId = table.Column<long>(type: "bigint", nullable: false),
+                    AnswerIndex = table.Column<long>(type: "bigint", nullable: false),
+                    QuizCorrectAnswersId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionCorrectAnswers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuestionCorrectAnswers_Questions_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "Questions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_QuestionCorrectAnswers_QuizCorrectAnswers_QuizCorrectAnswersId",
+                        column: x => x.QuizCorrectAnswersId,
+                        principalTable: "QuizCorrectAnswers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionCorrectAnswers_QuestionId",
+                table: "QuestionCorrectAnswers",
+                column: "QuestionId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionCorrectAnswers_QuizCorrectAnswersId",
+                table: "QuestionCorrectAnswers",
+                column: "QuizCorrectAnswersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_QuizInfoId",
+                table: "Questions",
+                column: "QuizInfoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuizCorrectAnswers_QuizId",
+                table: "QuizCorrectAnswers",
+                column: "QuizId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QuestionCorrectAnswers");
+
+            migrationBuilder.DropTable(
+                name: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "QuizCorrectAnswers");
+
+            migrationBuilder.DropTable(
+                name: "Quizzes");
+        }
+    }
+}
---
+++ 4-quiz/Quiz.Host/Model/QuizDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Quiz.Model;
+
+namespace Quiz.Host.Model;
+
+public class QuizDbContext(DbContextOptions<QuizDbContext> options) : DbContext(options)
+{
+    public DbSet<QuestionInfo> Questions => Set<QuestionInfo>();
+
+    public DbSet<QuizInfo> Quizzes => Set<QuizInfo>();
+
+    public DbSet<QuestionCorrectAnswer> QuestionCorrectAnswers => Set<QuestionCorrectAnswer>();
+
+    public DbSet<QuizCorrectAnswers> QuizCorrectAnswers => Set<QuizCorrectAnswers>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<QuizInfo>()
+            .HasMany<QuestionInfo>(nameof(QuizInfo.Questions));
+
+        modelBuilder.Entity<QuizCorrectAnswers>()
+            .HasMany<QuestionCorrectAnswer>(nameof(Quiz.Model.QuizCorrectAnswers.AnswersMap));
+
+        modelBuilder.Entity<QuizCorrectAnswers>()
+            .HasOne<QuizInfo>(a => a.Quiz)
+            .WithOne(q => q.CorrectAnswers)
+            .HasForeignKey<QuizCorrectAnswers>(a => a.QuizId);
+        
+        modelBuilder.Entity<QuestionCorrectAnswer>()
+            .HasOne<QuestionInfo>(a => a.QuestionInfo)
+            .WithOne(q => q.CorrectAnswer)
+            .HasForeignKey<QuestionCorrectAnswer>(a => a.QuestionId);
+    }
+}
