d372b7aec0cd848111bb29aa554a75240c4b3744
sunnyyssh <blit228@mail.ru>
2024-08-03 13:59:20 +0300

    Created WebAssembly project and added a reference from host project.

--- 4-quiz/Quiz.Host/Program.cs
+++ 4-quiz/Quiz.Host/Program.cs
@@ -3,4 +3,9 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.UseStaticFiles();
+
+app.UseBlazorFrameworkFiles("/webassembly");
+app.MapFallbackToFile("/webassembly/{*path:nonfile}", "/webassembly/index.html");
+
 app.Run();
