c2bc8ddede3ca4e585353ef0bcdd5fa7bc027919
sunnyyssh <blit228@mail.ru>
2024-08-05 17:38:21 +0300

    Created tests for controllers of ZoosApi

---
+++ 5-zoo-service/ZooService.Tests/ZoosApiTests/TicketsControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using ZooService.Model.Zoo;
+using ZooService.ZoosApi;
+
+namespace ZooService.Tests;
+
+public class TicketsControllerTests
+{
+    [Fact]
+    public async Task Gets_All_Without_Connected_Entities()
+    {
+        // Arrange
+        var mockRepository = new Mock<IRepository<Ticket>>();
+        mockRepository.Setup(r => r.GetAllAsync(true)).ReturnsAsync([]);
+        mockRepository.Setup(r => r.GetAllAsync(false)).ReturnsAsync([]);
+
+        var controller = new TicketsController(mockRepository.Object);
+
+        // Act
+        _ = await controller.GetAllAsync();
+
+        // Assert
+        mockRepository.Verify(r => r.GetAllAsync(true), Times.Never);
+        mockRepository.Verify(r => r.GetAllAsync(false), Times.Once);
+    }
+
+    [Fact]
+    public async Task Gets_One_With_Connected_Entities()
+    {
+        // Arrange
+        const int ticketId = 52;
+        var mockRepository = new Mock<IRepository<Ticket>>();
+        mockRepository.Setup(r => r.GetByIdAsync(ticketId, true)).ReturnsAsync(new Ticket());
+        mockRepository.Setup(r => r.GetByIdAsync(ticketId, false)).ReturnsAsync(new Ticket());
+
+        var controller = new TicketsController(mockRepository.Object);
+
+        // Act
+        _ = await controller.GetByIdAsync(ticketId);
+
+        // Assert
+        mockRepository.Verify(r => r.GetByIdAsync(ticketId, true), Times.Never);
+        mockRepository.Verify(r => r.GetByIdAsync(ticketId, false), Times.Once);
+    }
+}
---
+++ 5-zoo-service/ZooService.Tests/ZoosApiTests/ZoosControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using ZooService.Model.Zoo;
+using ZooService.ZoosApi;
+
+namespace ZooService.Tests;
+
+
+public class ZoosControllerTests
+{
+    [Fact]
+    public async Task Gets_All_Without_Connected_Entities()
+    {
+        // Arrange
+        var mockRepository = new Mock<IRepository<ZooInfo>>();
+        mockRepository.Setup(r => r.GetAllAsync(true)).ReturnsAsync([]);
+        mockRepository.Setup(r => r.GetAllAsync(false)).ReturnsAsync([]);
+
+        var controller = new ZoosController(mockRepository.Object);
+
+        // Act
+        _ = await controller.GetAllAsync();
+
+        // Assert
+        mockRepository.Verify(r => r.GetAllAsync(true), Times.Never);
+        mockRepository.Verify(r => r.GetAllAsync(false), Times.Once);
+    }
+
+    [Fact]
+    public async Task Gets_One_With_Connected_Entities()
+    {
+        // Arrange
+        const int zooId = 52;
+        var mockRepository = new Mock<IRepository<ZooInfo>>();
+        mockRepository.Setup(r => r.GetByIdAsync(zooId, true)).ReturnsAsync(new ZooInfo());
+        mockRepository.Setup(r => r.GetByIdAsync(zooId, false)).ReturnsAsync(new ZooInfo());
+
+        var controller = new ZoosController(mockRepository.Object);
+
+        // Act
+        _ = await controller.GetByIdAsync(zooId);
+
+        // Assert
+        mockRepository.Verify(r => r.GetByIdAsync(zooId, true), Times.Never);
+        mockRepository.Verify(r => r.GetByIdAsync(zooId, false), Times.Once);
+    }
+}
