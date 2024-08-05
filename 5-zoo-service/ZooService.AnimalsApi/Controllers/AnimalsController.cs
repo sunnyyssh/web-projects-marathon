ff4dd29932132753a87d8868d4a74ef482bfd2c6
sunnyyssh <blit228@mail.ru>
2024-08-05 13:54:12 +0300

    Add non-implemented animals api project

---
+++ 5-zoo-service/ZooService.AnimalsApi/Controllers/AnimalsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZooService.Model.Animals;
+
+namespace ZooService.AnimalsApi;
+
+[ApiController]
+[Route("/api/animals")]
+public class AnimalsController(IAnimalsRepository animalsRepository) : ControllerBase
+{
+    [HttpGet("all")]
+    public async Task<IEnumerable<AnimalInfo>> GetAllAsync()
+    {
+        return await animalsRepository.GetAllAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdAsync(long id)
+    {
+        return await animalsRepository.GetByIdAsync(id) is { } animal ? Ok(animal) : NotFound();
+    }
+
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateAsync(AnimalInfo animal) 
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        await animalsRepository.CreateAsync(animal);
+
+        return Created();
+    }
+
+    [HttpPut("update")]
+    public async Task<IActionResult> UpdateAsync(AnimalInfo animal)
+    {
+        if (!ModelState.IsValid) 
+        {
+            return BadRequest(ModelState);
+        }
+
+        bool success = await animalsRepository.UpdateAsync(animal);
+
+        return success ? Ok() : BadRequest();
+    }
+
+    [HttpDelete("delete/{id}")]
+    public async Task<IActionResult> DeleteAsync(long id)
+    {
+        bool success = await animalsRepository.DeleteAsync(id);
+        
+        return success ? Ok() : BadRequest();
+    }
+}
---
+++ 5-zoo-service/ZooService.AnimalsApi/Model/IAnimalsRepository.cs
@@ -0,0 +1,16 @@
+﻿using ZooService.Model.Animals;
+
+namespace ZooService.AnimalsApi;
+
+public interface IAnimalsRepository
+{
+    Task<IEnumerable<AnimalInfo>> GetAllAsync(bool loadAdditionalInfo = false);
+
+    Task<AnimalInfo?> GetByIdAsync(long id, bool loadAdditionalInfo = false);
+
+    Task<bool> UpdateAsync(AnimalInfo animal);
+
+    Task CreateAsync(AnimalInfo animal);
+
+    Task<bool> DeleteAsync(long id);
+}
