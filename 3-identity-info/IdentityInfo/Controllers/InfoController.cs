fbd52260a92167f8f5fd608e75a540b58c578b8f
sunnyyssh <blit228@mail.ru>
2024-08-05 15:01:51 +0300

    Add uncomplete implementation

---
+++ 3-identity-info/IdentityInfo/Controllers/InfoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityInfo.Controllers;
+
+[Authorize]
+public class InfoController(UserManager<IdentityUser> userManager) : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        var user = await userManager.GetUserAsync(User);
+        return View(user);
+    }
+}
---
+++ 3-identity-info/IdentityInfo/Model/ApiUserStore.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityInfo.Model;
+
+public class ApiUserStore : IUserStore<IdentityUser>
+{
+    public Task<string> GetUserIdAsync(IdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string?> GetUserNameAsync(IdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetUserNameAsync(IdentityUser user, string? userName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string?> GetNormalizedUserNameAsync(IdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetNormalizedUserNameAsync(IdentityUser user, string? normalizedName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IdentityResult> CreateAsync(IdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IdentityResult> UpdateAsync(IdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IdentityResult> DeleteAsync(IdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IdentityUser?> FindByIdAsync(string userId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IdentityUser?> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    { }
+}
