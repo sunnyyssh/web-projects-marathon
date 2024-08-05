9663779765800f4855bf8b79bb4e0690dbeb87d1
sunnyyssh <blit228@mail.ru>
2024-08-05 16:47:40 +0300

    Initialized empty ZoosApi

---
+++ 5-zoo-service/ZooService.ZoosApi/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using ZooService.ZooApi.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
