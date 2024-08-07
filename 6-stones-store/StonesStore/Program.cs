df3f62d5cda0dc85fcc38c4c5f192aec60102cc8
sunnyyssh <blit228@mail.ru>
2024-08-07 18:21:29 +0300

    Did just something

--- 6-stones-store/StonesStore/Program.cs
+++ 6-stones-store/StonesStore/Program.cs
@@ -1,6 +1,32 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StonesStore;
+using StonesStore.Model;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ConsumerIdentityContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration.GetConnectionString("ConsumerIdentityConnection"));
+    if (builder.Environment.IsDevelopment())
+    {
+        opts.EnableSensitiveDataLogging();
+    }
+});
+
+builder.Services.AddIdentity<ConsumerIdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ConsumerIdentityContext>();
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
