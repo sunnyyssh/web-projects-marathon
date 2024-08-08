77830e76abdae04ace71d617f8f06e960743c5c6
sunnyyssh <blit228@mail.ru>
2024-08-08 10:50:37 +0300

    Initial commit in SimpleShop

---
+++ 8-simple-shop/SimpleShop/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleShop.Controllers;
+
+[Route("/")]
+[Route("/home")]
+public class HomeController : Controller
+{
+    [Route("")]
+    [Route("index")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ 8-simple-shop/SimpleShop/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.MapRazorPages();
+app.MapControllers();
+
+app.Run();
